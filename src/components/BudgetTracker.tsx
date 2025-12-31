206bd401b6e7a7c603b90ae242c55251c908987c
Luis-Alvarezz <luisangelug10@gmail.com>
2025-12-30 22:05:46 -0600

    [MODIFY]: Cambio de PROP al 'presupuesto' a string

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -2,12 +2,43 @@
 import React from 'react'
 import { Image, StyleSheet, Text, View } from 'react-native'
 import globalStyles from '../styles'
+import AmountDisplay from './AmountDisplay'
 
-export default function BudgetTracker() {
+
+type BudgetTrackerProp = {
+  presupuesto: string
+}
+
+export default function BudgetTracker({ presupuesto }: BudgetTrackerProp) {
   return (
     <View style={styles.contenedor}>
       <View style={styles.centrarGrafica}>
         <Image source={ require('../img/grafico.jpg') } style={styles.imagen} />
+      </View>
+
+      <View style={styles.contenedorTexto}>
+
+        <Text>
+          <AmountDisplay
+            label="Budget"
+            amount={presupuesto}
+          />
+        </Text>
+
+        <Text>
+          <AmountDisplay
+            label="Availability"
+            amount={presupuesto}
+          />
+        </Text>
+
+        <Text>
+          <AmountDisplay
+            label="Spent"
+            amount={presupuesto}
+          />
+        </Text>
+
       </View>
     </View>
   )
@@ -25,5 +56,9 @@ const styles = StyleSheet.create({
   imagen: {
     width: 250,
     height: 250,
+  },
+
+  contenedorTexto: {
+    marginTop: 50,
   }
 })
