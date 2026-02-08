8b3cda5ee9ec025e714c1b54ca98625e79cf9953
Luis-Alvarezz <luisangelug10@gmail.com>
2026-02-08 13:00:04 -0600

    [ADD]: Componente para Mostrar Listado de Gastos

---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,53 @@
+// * rafc -> 
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import { Gasto } from '../types/type'
+import { ExpenseDetail } from './ExpenseDetail'
+
+type ExpenseListProp = {
+  gastos: Gasto[]
+}
+
+export const ExpenseList = ({ gastos }: ExpenseListProp) => {
+
+  const isEmpty = !gastos || gastos.length === 0
+  
+  return (
+    <View style={styles.contenedor}>
+      <Text style={styles.titulo}>Listado de Gastos</Text>
+      { isEmpty ? 
+        <Text style={styles.noGastoTitulo}>No hay gastos</Text> :
+        gastos.map(gasto => {
+          return (
+            <ExpenseDetail
+              key={gasto.id}
+              gasto={gasto}
+            />
+          ) 
+        })  
+      }
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  contenedor: {
+    marginTop: 40,
+    marginBottom: 100,
+  },
+  titulo: {
+    color: '#64748B',
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: '700',
+    marginTop: 50,
+  },
+  noGastoTitulo: {
+    marginTop: 20,
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#E53E3E',
+    fontWeight: '500',
+  }
+})
